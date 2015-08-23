3b858002623541ee05fce20a03da2b066612d1c2
jendib <benjamin.gam@gmail.com>
2015-08-23 20:47:01 +0200

    Closes #1: Display new videos with another style

--- src/main/resources/client/app/controller/Main.js
+++ src/main/resources/client/app/controller/Main.js
@@ -5,7 +5,13 @@
  */
 angular.module('mytube').controller('Main', function($scope, Restangular, Firebase, $rootScope) {
   $scope.watchLaterPlaylist = Firebase.getPlaylist('WL');
+  $scope.viewedVideos = Firebase.getViewedVideos();
   $scope.error = false;
+  $scope.firebaseReady = false;
+
+  $scope.viewedVideos.$loaded(function() {
+    $scope.firebaseReady = true;
+  });
 
   $scope.watchlater = function(video) {
     var inPlaylist = _.where($scope.watchLaterPlaylist, { id: video.id });
@@ -29,6 +35,36 @@ angular.module('mytube').controller('Main', function($scope, Restangular, Fireba
     });
   };
 
+  var syncViewedVideos = function() {
+    if (!$scope.data) {
+      return;
+    }
+
+    $scope.viewedVideos.$loaded(function() {
+      console.log('syncing viewed videos');
+
+      var viewedVideosId = _.pluck($scope.viewedVideos, '$value');
+
+      _.each($scope.data.videos, function(video) {
+        video.viewed = viewedVideosId.indexOf(video.id) != -1;
+
+        if (!video.viewed) {
+          console.log('adding video ' + video.id + ' to viewed videos');
+          $scope.viewedVideos.$add(video.id);
+        }
+      });
+
+
+      var videosId = _.pluck($scope.data.videos, 'id');
+      for (var i = 0; i < $scope.viewedVideos.length; i++) {
+        if (videosId.indexOf($scope.viewedVideos[i].$value) == -1) {
+          console.log('video ' + $scope.viewedVideos[i].$value + ' (' + i + ') not in actual data, cleaning up');
+          $scope.viewedVideos.$remove(i);
+        }
+      }
+    });
+  };
+
   $scope.watchLaterPlaylist.$watch(function() {
     $scope.$apply(function() {
       syncWatchLater();
@@ -42,6 +78,8 @@ angular.module('mytube').controller('Main', function($scope, Restangular, Fireba
       $scope.error = false;
       $scope.data = data;
       $rootScope.time = data.time;
+
+      syncViewedVideos();
       syncWatchLater();
     }, function () {
       $scope.error = true;
--- src/main/resources/client/app/service/Firebase.js
+++ src/main/resources/client/app/service/Firebase.js
@@ -4,9 +4,16 @@
  * Firebase service.
  */
 angular.module('mytube').factory('Firebase', function($firebaseArray) {
+  var firebaseUrl = 'https://FIREBASE_ID.firebaseio.com/mytube';
+
   return {
     getPlaylist: function(playlistId) {
-      var ref = new Firebase("https://FIREBASE_ID.firebaseio.com/mytube/playlist/" + playlistId);
+      var ref = new Firebase(firebaseUrl + '/playlist/' + playlistId);
+      return $firebaseArray(ref);
+    },
+
+    getViewedVideos: function() {
+      var ref = new Firebase(firebaseUrl + '/viewed_videos');
       return $firebaseArray(ref);
     }
   }
