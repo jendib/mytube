d331110e324602c4997e4350c85cf3195eca96de
bgamard <b.gamard@sismics.com>
2026-04-03 00:47:20 +0200

    remove TailwindCSS configuration and dependencies; update Node.js and other project dependencies

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
